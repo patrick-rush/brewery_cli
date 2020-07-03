f5200af26c52c737183fab84c0bd742fcfa4b956
patrick-rush <patrick.a.rush@gmail.com>
2020-07-02 21:29:22 -0400

    filetree setup and gemspec

--- lib/brewery_cli.rb
+++ lib/brewery_cli.rb
@@ -1,4 +1,7 @@
-require "brewery_cli/version"
+require_relative "brewery_cli/version"
+require_relative "brewery_cli/api"
+require_relative "brewery_cli/cli"
+require_relative "brewery_cli/brewery"
 
 module BreweryCli
   class Error < StandardError; end
